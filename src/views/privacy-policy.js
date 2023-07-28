4b63f76718f02c72b142898c379be32abf66e298
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2023-07-28 22:20:46 +0100

    react

---
+++ src/views/privacy-policy.js
@@ -0,0 +1,236 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './privacy-policy.css'
+
+const PrivacyPolicy = (props) => {
+  return (
+    <div className="privacy-policy-container">
+      <Helmet>
+        <title>Privacy-Policy - Keep everything Planned</title>
+        <meta
+          property="og:title"
+          content="Privacy-Policy - Keep everything Planned"
+        />
+      </Helmet>
+      <div className="privacy-policy-container1">
+        <h1 className="privacy-policy-text">
+          Privacy Policy for Keep everything Planned
+        </h1>
+        <p className="privacy-policy-text01">
+          At Planner support, accessible from planner-support.com, one of our
+          main priorities is the privacy of our visitors. This Privacy Policy
+          document contains types of information that is collected and recorded
+          by Planner support and how we use it
+        </p>
+        <p className="privacy-policy-text02">
+          If you have additional questions or require more information about our
+          Privacy Policy, do not hesitate to contact us
+        </p>
+        <p className="privacy-policy-text03">
+          This Privacy Policy applies only to our online activities and is valid
+          for visitors to our website with regards to the information that they
+          shared and/or collect in Planner support. This policy is not
+          applicable to any information collected offline or via channels other
+          than this website
+        </p>
+        <h2 className="privacy-policy-text04">Consent</h2>
+        <p className="privacy-policy-text05">
+          By using our website, you hereby consent to our Privacy Policy and
+          agree to its terms
+        </p>
+        <h2 className="privacy-policy-text06">Information we collect</h2>
+        <p className="privacy-policy-text07">
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at
+          the point we ask you to provide your personal information
+        </p>
+        <p className="privacy-policy-text08">
+          If you contact us directly, we may receive additional information
+          about you such as your name, email address, phone number, the contents
+          of the message and/or attachments you may send us, and any other
+          information you may choose to provide
+        </p>
+        <p className="privacy-policy-text09">
+          When you register for an Account, we may ask for your contact
+          information, including items such as name, company name, address,
+          email address, and telephone number
+        </p>
+        <h2 className="privacy-policy-text10">How we use your information</h2>
+        <p className="privacy-policy-text11">
+          We use the information we collect in various ways, including to:
+        </p>
+        <ul className="privacy-policy-ul list">
+          <li className="privacy-policy-li list-item">
+            <span>Provide, operate, and maintain our website</span>
+          </li>
+          <li className="privacy-policy-li1 list-item">
+            <span>Improve, personalize, and expand our website</span>
+          </li>
+          <li className="privacy-policy-li2 list-item">
+            <span>Understand and analyze how you use our website</span>
+          </li>
+          <li className="privacy-policy-li3 list-item">
+            <span>
+              Develop new products, services, features, and functionality,
+              operate, and maintain our website
+            </span>
+          </li>
+          <li className="privacy-policy-li4 list-item">
+            <span>
+              Communicate with you, either directly or through one of our
+              partners, including for customer service, to provide you with
+              updates and other information relating to the website, and for
+              marketing and promotional purposes
+            </span>
+          </li>
+          <li className="privacy-policy-li5 list-item">
+            <span>Send you emails</span>
+          </li>
+          <li className="privacy-policy-li6 list-item">
+            <span>Find and prevent fraud</span>
+          </li>
+        </ul>
+        <h2 className="privacy-policy-text19">Log Files</h2>
+        <p className="privacy-policy-text20">
+          Planner support follows a standard procedure of using log files. These
+          files log visitors when they visit websites. All hosting companies do
+          this and a part of hosting services&apos; analytics. The information
+          collected by log files include internet protocol (IP) addresses,
+          browser type, Internet Service Provider (ISP), date and time stamp,
+          referring/exit pages, and possibly the number of clicks. These are not
+          linked to any information that is personally identifiable. The purpose
+          of the information is for analyzing trends, administering the site,
+          tracking users&apos; movement on the website, and gathering
+          demographic information
+        </p>
+        <h2 className="privacy-policy-text21">
+          Advertising Partners Privacy Policies
+        </h2>
+        <p className="privacy-policy-text22">
+          You may consult this list to find the Privacy Policy for each of the
+          advertising partners of Planner support
+        </p>
+        <p className="privacy-policy-text23">
+          Third-party ad servers or ad networks uses technologies like cookies,
+          JavaScript, or Web Beacons that are used in their respective
+          advertisements and links that appear on Planner support, which are
+          sent directly to users&apos; browser. They automatically receive your
+          IP address when this occurs. These technologies are used to measure
+          the effectiveness of their advertising campaigns and/or to personalize
+          the advertising content that you see on websites that you visit
+        </p>
+        <p className="privacy-policy-text24">
+          Note that Planner support has no access to or control over these
+          cookies that are used by third-party advertisers
+        </p>
+        <h2 className="privacy-policy-text25">Third Party Privacy Policies</h2>
+        <p className="privacy-policy-text26">
+          Planner support&apos;s Privacy Policy does not apply to other
+          advertisers or websites. Thus, we are advising you to consult the
+          respective Privacy Policies of these third-party ad servers for more
+          detailed information. It may include their practices and instructions
+          about how to opt-out of certain options
+        </p>
+        <p className="privacy-policy-text27">
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers&apos;
+          respective websites
+        </p>
+        <h2 className="privacy-policy-text28">
+          CCPA Privacy Rights (Do Not Sell My Personal Information)
+        </h2>
+        <p className="privacy-policy-text29">
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+        </p>
+        <p className="privacy-policy-text30">
+          Request that a business that collects a consumer&apos;s personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers
+        </p>
+        <p className="privacy-policy-text31">
+          Request that a business delete any personal data about the consumer
+          that a business has collected
+        </p>
+        <p className="privacy-policy-text32">
+          Request that a business that sells a consumer&apos;s personal data,
+          not sell the consumer&apos;s personal data
+        </p>
+        <p className="privacy-policy-text33">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us
+        </p>
+        <h2 className="privacy-policy-text34">GDPR Data Protection Rights</h2>
+        <p className="privacy-policy-text35">
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+        </p>
+        <p className="privacy-policy-text36">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service
+        </p>
+        <p className="privacy-policy-text37">
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete
+        </p>
+        <p className="privacy-policy-text38">
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions
+        </p>
+        <p className="privacy-policy-text39">
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions
+        </p>
+        <p className="privacy-policy-text40">
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions
+        </p>
+        <p className="privacy-policy-text41">
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions
+        </p>
+        <p className="privacy-policy-text42">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us
+        </p>
+        <h2 className="privacy-policy-text43">Children&apos;s Information</h2>
+        <p className="privacy-policy-text44">
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity
+        </p>
+        <p className="privacy-policy-text45">
+          Planner support does not knowingly collect any Personal Identifiable
+          Information from children under the age of 13. If you think that your
+          child provided this kind of information on our website, we strongly
+          encourage you to contact us immediately and we will do our best
+          efforts to promptly remove such information from our records
+        </p>
+        <h2 className="privacy-policy-text46">
+          Changes to This Privacy Policy
+        </h2>
+        <p className="privacy-policy-text47">
+          We may update our Privacy Policy from time to time. Thus, we advise
+          you to review this page periodically for any changes. We will notify
+          you of any changes by posting the new Privacy Policy on this page.
+          These changes are effective immediately, after they are posted on this
+          page
+        </p>
+        <h2 className="privacy-policy-text48">Contact Us</h2>
+        <p className="privacy-policy-text49">
+          If you have any questions or suggestions about our Privacy Policy, do
+          not hesitate to contact us
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
